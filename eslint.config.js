cb0417a73e1cf77ee768ac27a24d92ba3168f5ef
Migs <miguel@offshorly.com>
2024-09-12 20:28:05 +0800

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginReact from 'eslint-plugin-react'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  ...{
+    rules: {
+      semi: false,
+    },
+  },
+]
