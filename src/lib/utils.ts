fbf32d71cbe7a5b0ad1b6f8c9453791146881995
m1ggy <miguel.buising1@gmail.com>
2024-10-12 15:43:52 +0800

    disable proxy for now

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function proxyImage(url: string, referrer = window.location.origin) {
-  return `${import.meta.env.VITE_BASE_API_URL}/utils/image-proxy?url=${url}&headers={"referer": "${referrer}"}`
+  //DISABLE PROXY FOR NOW
+  // return `${import.meta.env.VITE_BASE_API_URL}/utils/image-proxy?url=${url}&headers={"referer": "${referrer}"}`
+  return url
 }
