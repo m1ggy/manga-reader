93de8952befff70bfa2cea8ebe262707b29d3bb7
m1ggy <miguel.buising1@gmail.com>
2024-10-12 15:21:38 +0800

    remove converting into blob for images

--- src/components/globals/page.tsx
+++ src/components/globals/page.tsx
@@ -1,7 +1,3 @@
-import { useQuery } from '@tanstack/react-query'
-import { useEffect } from 'react'
-import { Skeleton } from '../ui/skeleton'
-
 type PageProps = {
   src: string
   onClick: () => void
@@ -10,40 +6,10 @@ type PageProps = {
 }
 
 function Page({ src, onClick, mangaId, chapterId }: PageProps) {
-  const { data, isLoading } = useQuery<string>({
-    queryFn: () =>
-      new Promise((resolve) => {
-        fetch(
-          `${import.meta.env.VITE_BASE_API_URL}/utils/image-proxy?url=${src}&headers={"referer": "https://www.mangachows.com/${mangaId}/chapters/${chapterId}"}`,
-        )
-          .then((res) => res.blob())
-          .then((blob) => {
-            const url = URL.createObjectURL(blob)
-            resolve(url)
-          })
-      }),
-    queryKey: [src, 'page'],
-  })
-
-  useEffect(() => {
-    return () => {
-      if (data) {
-        URL.revokeObjectURL(data)
-      }
-    }
-  }, [])
-
-  if (isLoading || !data)
-    return (
-      <div className='w-full'>
-        <Skeleton className='rounded w-[70vh] h-[70vh]' />
-      </div>
-    )
-
   return (
     <div>
       <img
-        src={data}
+        src={`${import.meta.env.VITE_BASE_API_URL}/utils/image-proxy?url=${src}&headers={"referer": "https://www.mangachows.com/${mangaId}/chapters/${chapterId}"}`}
         className='w-full select-none'
         onContextMenu={(e) => e.preventDefault()}
         onClick={onClick}
